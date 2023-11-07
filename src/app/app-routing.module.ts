73da8bbd5989d31bd2dfc815dc87717157cb236a
Fseiber <davidsseiber@gmail.com>
2023-11-06 21:24:50 -0300

    Se agrego proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SeriesComponent } from './components/routes/series/series.component';
+import { IngresarComponent } from './components/routes/ingresar/ingresar.component';
+import { InicioComponent } from './components/routes/inicio/inicio.component';
+import { PeliculasComponent } from './components/routes/peliculas/peliculas.component';
+
+const routes: Routes = [
+  {
+    path: 'Inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'Peliculas',
+    component: PeliculasComponent
+  },
+  {
+    path: 'Series',
+    component: SeriesComponent
+  },
+  {
+    path: 'Ingresar',
+    component: IngresarComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
